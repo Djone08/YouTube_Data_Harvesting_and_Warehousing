0ca3269e177bcf498f51f4249711e26b9f345d5e
Djone <devjoneraj8@gmail.com>
2024-04-23 21:49:49 +0530

    update 23_04_2024

---
+++ Test.py
@@ -0,0 +1,102 @@
+from googleapiclient.discovery import build
+import pandas as pd
+import numpy as np
+from typing import Literal
+import streamlit as st
+
+
+class YTAPI(object):
+    # def __new__(cls, *args, **kwargs):
+    #     cls.__init__(*args, **kwargs)
+    #     pass
+    #
+    def __init__(self, _api_keys: list[str]):
+        self.yt_apis = [build('youtube',
+                              'v3', developerKey=x) for x in _api_keys]
+
+    def search_list(self, text: str,
+                    typ: Literal['channel', 'playlist', 'video'] = 'channel'):
+        _res = self.yt_apis[0].search().list(
+            part='snippet',
+            # fields='nextPageToken,prevPageToken,items(snippet(channelId,thumbnails(default),channelTitle))',
+            type=typ,
+            maxResults=50,
+            q=text).execute()
+        return _res
+
+    def channel_list(self, _channel_id: str):
+        _res = self.yt_apis[0].channels().list(
+            part='snippet,contentDetails,statistics',
+            # fields='nextPageToken,prevPageToken,items(snippet(channelId,thumbnails(default),channelTitle))',
+            id=_channel_id).execute()
+        return _res
+
+
+if __name__ == '__main__':
+    api_key = 'AIzaSyAhcBInG7i36z7TYEygtIkMLCnm73YBf14'
+    channel_id = 'UCiEmtpFVJjpvdhsQ2QAhxVA'
+    yt = YTAPI([api_key])
+
+    # st.write(yt.search_list(channel_id))
+
+    '# Hi'
+    # st.text_input(label='Channel Search', placeholder='Search', key='channel_search')
+    # if st.button(label='Search', disabled=not bool(st.session_state.channel_search)):
+    #     _df = pd.DataFrame(yt.search_list(st.session_state.channel_search, 'channel')['items'])
+    #     _df = _df.apply(lambda x: x.snippet, axis=1, result_type='expand')
+    #     _df.loc[:, 'logo'] = _df.thumbnails.apply(lambda x: x['default']['url'])
+    #     _df.loc[:, 'check'] = False
+    #     st.data_editor(_df,
+    #                    column_config={
+    #                        'check': st.column_config.CheckboxColumn('Check', default=False),
+    #                        'logo': st.column_config.ImageColumn("Logo")
+    #                    }
+    #                    )
+    #
+    # st.write("Here's our first attempt at using data to create a table:")
+    # df = pd.DataFrame({
+    #     'first column': [1, 2, 3, 4],
+    #     'second column': [10, 20, 30, 40]
+    # })
+    # st.dataframe(df.style.highlight_min())
+    #
+    # if st.checkbox('Show dataframe'):
+    #     chart_data = pd.DataFrame(
+    #         np.random.randn(20, 3),
+    #         columns=['a', 'b', 'c'])
+    #
+    #     chart_data
+    #
+    # df = pd.DataFrame({
+    #     'first column': [1, 2, 3, 4],
+    #     'second column': [10, 20, 30, 40]
+    # })
+    #
+    # option = st.selectbox(
+    #     'Which number do you like best?',
+    #     df['first column'])
+    #
+    # st.write('You selected: ', option)
+    #
+    # add_selectbox = st.sidebar.selectbox(
+    #     'How would you like to be contacted?',
+    #     ('Email', 'Home phone', 'Mobile phone')
+    # )
+    #
+    # add_slider = st.sidebar.slider(
+    #     'Select a range of values',
+    #     0.0, 100.0, (25.0, 75.0)
+    # )
+    #
+    # left_column, right_column = st.columns(2)
+    # # You can use a column just like st.sidebar:
+    # left_column.button('Press me!')
+    #
+    # # Or even better, call Streamlit functions inside a "with" block:
+    # with right_column:
+    #     chosen = st.radio(
+    #         'Sorting hat',
+    #         ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"), key='hat')
+    #     f"You are in {st.session_state.hat} house!"
+    #
+    # 'hi there'
---
+++ pages/Add Channel.py
@@ -0,0 +1,116 @@
+import streamlit as st
+import pandas as pd
+from Test import YTAPI
+
+
+def on_search(_txt: str):
+    st.session_state.update({'chn_srh_hst': {}}) if not st.session_state.get('chn_srh_hst') else None
+    _data = yt.search_list(_txt, 'channel')
+    st.session_state.update(NextPageToken=data.get('nextPageToken', False))
+    _df = pd.DataFrame(_data['items'])
+    _df = _df.apply(lambda x: x.snippet, axis=1, result_type='expand')
+    _df.loc[:, 'logo'] = _df.thumbnails.apply(lambda x: x['default']['url'])
+    _df.loc[:, 'check'] = False
+    _df.to_csv('temp.csv', index=False)
+
+
+def set_row(_data: pd.Series, cont: st.delta_generator.DeltaGenerator):
+    with cont:
+        c1, c2, c3 = st.columns([.1, .2, .7])
+        with c1:
+            ''
+            ''
+        with c2:
+            st.write(f'''<p>
+            <img src="{_data.logo}"
+            alt="logo" title="{_data.channelTitle}" style="border-radius:50%" />
+            </p>
+            ''', unsafe_allow_html=True)
+        with c3:
+            st.write(f'### {_data.channelTitle}')
+            st.caption(f'{_data.description}')
+        st.divider()
+        return c1.checkbox(f'cb_{_data.channelTitle}', label_visibility='collapsed')
+
+
+
+api_key = 'AIzaSyBii7IbnVXI3CD1GIQ5tutU4bWmCxnVBHc'
+channel_id = 'UCiEmtpFVJjpvdhsQ2QAhxVA'
+yt = YTAPI([api_key])
+
+# st.write(yt.channel_list(channel_id))
+t1, t2 = st.tabs(['Add Channel by Name', 'Add Channel by ID'])
+with t1:
+    '# Channel Search'
+
+    srh_txt = st.text_input(label='Search Bar', placeholder='Search', label_visibility='collapsed')
+
+    if st.button(label='Search', disabled=not srh_txt):
+        df = pd.DataFrame(yt.search_list(srh_txt, 'channel')['items'])
+        df = df.apply(lambda x: x.snippet, axis=1, result_type='expand')
+        df.loc[:, 'logo'] = df.thumbnails.apply(lambda x: x['default']['url'])
+        df.loc[:, 'check'] = False
+        df.to_csv('temp.csv', index=False)
+    elif srh_txt:
+        df = pd.read_csv('temp.csv', index_col='channelId')
+    else:
+        df = pd.read_csv('temp.csv')
+        df = pd.DataFrame(columns=df.columns)
+        df.to_csv('temp.csv', index=False)
+
+    if not df.empty:
+        st.button('Get Channel Details')
+        # st.markdown('''<style><\style>''')
+        cont = st.container()
+        cb_df = df.apply(lambda x: set_row(x, cont), axis=1)
+        df['check'] = cb_df
+        cols = st.columns([.7, .2])
+        cols[0].write('[Go Up :arrow_up:](#channel-search)')
+        cols[1].button('See More...')
+        # print(df[_df])
+
+with t2:
+    '# Add Channel by ID'
+
+    ch_id = st.text_input(label='Channel ID', placeholder='Channel ID', label_visibility='collapsed').strip()
+    id_chk = ch_id.replace('_', '0').replace('-', '0')
+
+    btn_col = st.columns([.1, .4, .5])
+
+    add_btn = btn_col[0].button(label='Add', disabled=not (id_chk.isalnum() and len(ch_id) == 24))
+
+    with btn_col[1]:
+        if ch_id and len(ch_id) != 24:
+            st.caption(':red[The Channel ID must be of 24 Characters]')
+        elif ch_id and not id_chk.isalnum():
+            st.caption(':red[The Channel ID con not Contain Spaces or '
+                       'Any other Special Characters except ( "_" , "-" )]')
+
+    if add_btn:
+        data = yt.channel_list(ch_id)['items'][0]
+        logo = data['snippet']['thumbnails']['default']['url']
+        title = data['snippet']['title']
+        description = data['snippet']['description']
+
+        cols = st.columns(3)
+
+        with cols[0]:
+            st.image(logo)
+        with cols[1]:
+            f'# {title}'
+            st.caption(description)
+        st.success('yeay!')
+        st.error('This is an error', icon='🚨')
+        # print(bool(st.session_state.get('new_')))
+    import time
+    em = st.empty()
+    with em.status("Downloading data...", expanded=False) as status:
+        st.write("Searching for data...")
+        time.sleep(2)
+        st.write("Found URL.")
+        time.sleep(1)
+        st.success("Downloading data...")
+        time.sleep(1)
+        status.update(label="Download complete!", state="complete", expanded=False)
+    time.sleep(1)
+    em.empty()
